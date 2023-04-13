46980807b1b30d54a680dfe92462044fff60d349
Jacob Hummer <jcbhmr@outlook.com>
2023-04-12 20:50:58 -0500

    Add alert() confirm() too

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-// import alert from "./alert.ts";
-// import confirm from "./confirm.ts";
+import alert from "./alert.ts";
+import confirm from "./confirm.ts";
 import prompt from "./prompt.ts";
 
+type alert_ = typeof alert;
+type confirm_ = typeof confirm;
 type prompt_ = typeof prompt;
 declare global {
-  // var alert: typeof alert;
-  // var confirm: typeof confirm;
+  // @ts-ignore
+  var alert: alert_;
+  // @ts-ignore
+  var confirm: confirm_;
   // @ts-ignore
   var prompt: prompt_;
 }
 
-// globalThis.alert = alert;
-// globalThis.confirm = confirm;
+globalThis.alert = alert;
+globalThis.confirm = confirm;
 globalThis.prompt = prompt;
